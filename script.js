b8f4b6014437dcac802f6f3c7d1ab5f78070ce28
Nancy-Sabrina-Ishimwe <ishimwenancysabrina1@gmail.com>
2023-12-11 16:44:14 +0200

    changes on the function in the script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
-async function getWeather(){
-    var res = await fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=london&appid=77b551fe2297c66195e5d34f598a48ad&units=metric');
-    var data = await res.json();
-    console.log(data);
-    document.querySelector('.celcius').innerHTML = data.main.temp;
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.humidityP').innerHTML = data.main.humidity;
-    document.querySelector('.windS').innerHTML = data.wind.speed;
-        }
-
-        getWeather();
+function searchWeather() {
+    var city = document.querySelector('input[type="text"]').value;
+    var apikey ='77b551fe2297c66195e5d34f598a48ad';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
+      .then(response => response.json())
+      .then(data => {
+        document.querySelector('.celcius').innerHTML = data.main.temp;
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.humidityP').innerHTML = data.main.humidity;
+        document.querySelector('.windS').innerHTML = data.wind.speed;
+      })
+      .catch(error => console.error('Error:', error));
+   }
+   
